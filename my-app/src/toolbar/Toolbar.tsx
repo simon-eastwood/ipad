10e83b97b5b214f0be6e200e10b00612f329db86
Simon Eastwood <seastwood@epo.org>
2021-06-25 17:05:26 +0200

    upload to gitghub

--- my-app/src/toolbar/Toolbar.tsx
+++ my-app/src/toolbar/Toolbar.tsx
@@ -7,7 +7,7 @@ const toolbarStyles = {
 
 const Toolbar = () => {
     return (
-        <div style={toolbarStyles}><Header appName="Workbench" logo="/EPO-logo.034e00d7.gif" backgroundColor="white" leftSide={<div style={{ display: 'flex', marginLeft: '6rem' }}>left</div>} rightSide={<div style={{ display: 'flex', marginRight: '6rem' }}>right</div>} />
+        <div style={toolbarStyles}><Header appName="Workbench" logo="./EPO-logo.034e00d7.gif" backgroundColor="white" leftSide={<div style={{ display: 'flex', marginLeft: '6rem' }}>left</div>} rightSide={<div style={{ display: 'flex', marginRight: '6rem' }}>right</div>} />
         </div>
     )
 }
